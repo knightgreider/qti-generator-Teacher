885c6eca7b6fa6a28ecb7db36ce5b60bdfc9cbd9
knightgreider <williamgreider@mac.com>
2025-05-13 13:43:15 -0400

    May 13th - 22

--- components/QTIQuizGenerator.js
+++ components/QTIQuizGenerator.js
@@ -108,7 +108,7 @@ ${q.choices.map((c,j) => `              <response_label ident="choice${j+1}"><ma
     // placeholders
     ['context.xml','course_settings.xml','files_meta.xml','media_tracks.xml'].forEach(name => {
       const tag = name.split('.')[0];
-      zip.file(name, `<${tag}/>');
+      zip.file(name, `<${tag}/>`) ;
     });
 
     const blob = await zip.generateAsync({ type: 'blob' });
